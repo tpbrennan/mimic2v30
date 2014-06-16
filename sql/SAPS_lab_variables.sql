dbfd42920f02e89575e9974f773a8a8eaad4471d
tpbrennan <tombombadl@gmail.com>
2014-06-16 14:07:04 -0400

    split SAPS

--- sql/SAPS_lab_variables.sql
+++ sql/SAPS_lab_variables.sql
@@ -1,5 +1,5 @@
 drop materialized view tbrennan.saps_labvars_mimic2v30;
-create materialized view tbrennan.saps_labvars_mimic2v30 as
+--create materialized view tbrennan.saps_labvars_mimic2v30 as
 
 with all_icustay_days as (
 
@@ -55,38 +55,37 @@ with all_icustay_days as (
             when c.itemid in (50916)
               and c.charttime between s.begintime and s.endtime then 'CREATININE'
           end as category,
-          valuenum
+          value
           
    from all_icustay_days s
-   join mimic2v30.labevents c 
-     on s.icustay_id = c.icustay_id
+   join mimic2v30.labevents c on s.subject_id = c.subject_id
    where c.charttime between s.begintime and s.endtime
  
    and ((c.itemid in (51243,50809) -- 'HCT'
-          and c.valuenum between 5 and 100) -- 0 <> 390
+          and c.value between 5 and 100) -- 0 <> 390
  
         or (c.itemid in  (51327,51326) -- 'WBC' 
-          and c.valuenum between 5 and 1000000) -- 0 <> 1,250,000
+          and c.value between 5 and 1000000) -- 0 <> 1,250,000
  
         or (c.itemid in (50936) -- 'GLUCOSE'  
-          and c.valuenum between 0.5 and 1000) -- -251 <> 3555
+          and c.value between 0.5 and 1000) -- -251 <> 3555
  
         or (c.itemid in (50886,50803,50802)--'HCO3'
-          and c.valuenum between 2 and 100) -- 0 <> 231
+          and c.value between 2 and 100) -- 0 <> 231
  
         or (c.itemid in (50976,50821) -- 'POTASSIUM'
-          and c.valuenum between 0.5 and 70) -- 0.7	<> 52
+          and c.value between 0.5 and 70) -- 0.7	<> 52
  
         or (c.itemid in (50989,50823)-- 'SODIUM'
-          and c.valuenum between 50 and 300) -- 1.07 <>	1332
+          and c.value between 50 and 300) -- 1.07 <>	1332
  
         or (c.itemid in (51011) -- 'BUN'
-          and c.valuenum between 1 and 100) -- 0 <> 280
+          and c.value between 1 and 100) -- 0 <> 280
         
         or (c.itemid in (50916) -- 'CREATININE'
-          and c.valuenum between 0 and 30) -- 0	<> 73
+          and c.value between 0 and 30) -- 0	<> 73
      )
-     and c.valuenum is not null
+     and c.value is not null
   )
 --select * from LabParams;
 
