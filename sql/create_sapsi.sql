01d2370c08d6fda4f7b6e27ed8b665c528f9b934
tpbrennan <tombombadl@gmail.com>
2014-06-18 09:27:42 -0400

    sapsi analysis

--- sql/create_sapsi.sql
+++ sql/create_sapsi.sql
@@ -1,6 +1,6 @@
 drop materialized view tbrennan.mimic2v30_sapsi;
 
---create materialized view tbrennan.mimic2v30_sapsi as 
+create materialized view tbrennan.mimic2v30_sapsi as 
 
 with final_sapsi as (
   select distinct subject_id,
@@ -14,61 +14,7 @@ with final_sapsi as (
     where param_count > 15
     order by subject_id
 )
+select * from final_sapsi;
 --select count(distinct subject_id) from final_sapsi; -- 12411 subjects
 --select count(distinct icustay_id) from final_sapsi; -- 12411 subjects, 13969 icustays
 
-, compare_sapsi as (
-  select fs.*,
-    id.sapsi_first old_sapsi_first,
-    id.sapsi_min old_sapsi_min,
-    id.sapsi_max old_sapsi_max
-    from final_sapsi fs
-    left join mimic2v26.icustay_detail id
-      on fs.subject_id = id.subject_id
-     and fs.hadm_id = id.hadm_id
-)
---select * from compare_sapsi;
-
-, mortality as (
-  select cs.*,
-    ad.HOSPITAL_EXPIRE_FLG
-    from compare_sapsi cs 
-    join mornin.v30_admissions ad 
-      on cs.subject_id = ad.subject_id
-     and cs.hadm_id = ad.hadm_id
-)
-select * from mortality;
-
-
-, detailed_saps as (
-  -- onle select SAPS scores
-  select cs.subject_id,
-         cs.hadm_id,
-         cs.icustay_id,
-         cs.seq,
-         cs.lod,
-         cs.sapsi,
-         cs.param_count,
-         p.age,
-         p.hr,
-         p.temp,
-         p.sysabp,
-         p.vent_resp,
-         p.spon_resp,
-         p.bun,
-         p.hct,
-         p.wbc,
-         p.glucose,
-         p.k,
-         p.na,
-         p.hco3,
-         p.gcs,
-         p.urine
-    from calc_saps_score cs
-    join pivotparams p
-      on cs.subject_id = p.subject_id 
-     and cs.icustay_id = p.icustay_id
-     and cs.seq = p.seq
-    order by subject_id, icustay_id, seq
-)
---select count(distinct icustay_id) from final_saps;
